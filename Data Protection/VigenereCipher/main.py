5834bdb1204fb25b40db7b6cadc096a8aaffc946
kapp <lukaszwasyl75@gmail.com>
2019-04-01 02:13:13 +0200

    Almost Done Viginiere Cipher

--- Data Protection/VigenereCipher/main.py
+++ Data Protection/VigenereCipher/main.py
@@ -4,13 +4,19 @@ from vigenere import VigenereCipher
 
 def main():
     lst  = [' ','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
- 
+    pattern_container = []
+    cipher_container = []
     #final_table = [lst[i:]+lst[:i] for i in range(len(lst))]
     obj = VigenereCipher(lst)
-    obj.input('to_code.txt')
-    obj.encode('kasztan')
-    obj.decode('kasztan')
-    obj.frequency()
+    obj.input('pattern.txt', pattern_container)
+    obj.input('to_code.txt', cipher_container)
+
+
+
+
+    # obj.decode('ZAB')
+    obj.friedman_method(pattern_container,cipher_container)
+  
     
 
 if __name__ == "__main__":
--- Data Protection/VigenereCipher/vigenere.py
+++ Data Protection/VigenereCipher/vigenere.py
@@ -8,24 +8,24 @@ class VigenereCipher():
 
     def __init__(self, myDict):   
         self.dict = myDict
-        self.dataContainer = []
         self.cipher_text = []
         self.decoded_cipher = []
         self.cipher_text_coded = ''
+        self.to_decode = []
 
 
-    def input(self, choice):
+    def input(self, choice, container):
 
         """ Wprowadzanie pliku do zakodowania """
 
         file = open(choice)
         with file as file_handle:
             for line in file_handle:
-                self.dataContainer += line.upper()
+                container += line.upper()
         file.close()
 
 
-    def encode(self,key):
+    def encode(self,key, container):
 
         """ Szyfrowanie danych  """
 
@@ -33,7 +33,7 @@ class VigenereCipher():
         counter = 0
  
         key_val = [self.dict.index(x.upper()) if x.upper() in self.dict else x for x in key ]
-        text_val = [self.dict.index(x) if x in self.dict else x for x in self.dataContainer    ]
+        text_val = [self.dict.index(x) if x in self.dict else x for x in container    ]
    
         for x in text_val:
             if counter == len(key_val):
@@ -53,32 +53,86 @@ class VigenereCipher():
         f.close()
         return self.cipher_text_coded
 
-    def decode(self, key):
+    def decode(self, key, container):
 
         """ Odkodowywanie Danych  """
-
-        key_val = [self.dict.index(x.upper()) for x in key if x.upper() in self.dict]
-        text_val = [self.dict.index(x) if x in self.dict else x for x in self.cipher_text_coded ]
         
+        key_val = [self.dict.index(x.upper()) for x in key if x.upper() in self.dict]
+        text_val = [self.dict.index(x) if x in self.dict else x for x in container ]
+
+        print(key_val)
+        print(text_val)
         counter = 0
+
         for x in text_val:
             if counter == len(key_val):
                 counter = 0
             if isinstance(x,str):
-                x_val = x
-                self.decoded_cipher.append(x_val)
-            else:
-                self.decoded_cipher.append(((x - key_val[counter])) % 27 )
-                
-            
+                self.decoded_cipher.append(x)
+                counter -= 1
+            elif isinstance(x,int):
+                self.decoded_cipher.append(((x - key_val[counter] )) % 27 )
+             
             counter += 1
 
-        self.decoded_cipher_values = [ self.dict[x] if isinstance(x, int) else x  for x in  self.decoded_cipher]
+        self.decoded_cipher_values = [ self.dict[x] if isinstance(x, int) else x for x in  self.decoded_cipher]
 
+
+        
         f = open("decoded.txt", 'w')
         f.write(''.join(self.decoded_cipher_values))
         f.close()
         return self.decoded_cipher_values
+
+
+    def friedman_method(self, containerPattern, containerCipher ):
+        container_pattern = [x for x in containerPattern if x in self.dict ]
+        container_cipher = [x for x in containerCipher if x in self.dict ]
+
+        commonPat = self.mostCommon(container_pattern)
+        commonCip = self.mostCommon(container_cipher)
+
+        sumUp = float(sum([ x[1]*(x[1]-1) for x in commonPat]))
+
+        sumDown = float(sum([x[1] for x in commonPat]) *( sum([x[1] for x in commonPat]) - 1))
+
+        icPattern = float(sumUp/sumDown)
+
+        lst = []
+        lstOfIc = []
+
+        cipher_string = ''.join(container_cipher)
+
+          
+        for x in range(2,20):
+            h = x
+            tables = [[ x for x in cipher_string[i::h]] for i in range(h)]
+            lst.append(tables)
+        
+
+        avg = 0
+        for x in lst:
+            for y in x:
+                avg += self.indexOfCoincidence(y)
+                
+            elem = [ len(x),(avg/len(x))]
+            lstOfIc.append(elem)
+            avg = 0
+        
+        print(icPattern)
+        print(lstOfIc)
+
+
+
+    def indexOfCoincidence(self,text):
+
+        lst = self.mostCommon(text)
+        nominator = float(sum([ x[1]*(x[1]-1) for x in lst]))
+        denominator = float(sum([x[1] for x in lst]) * ( sum([x[1] for x in lst]) -1 ))
+        result = float(nominator/denominator)
+        return result
+    
+
 
     def mostCommon(self, text):
 
@@ -94,12 +148,21 @@ class VigenereCipher():
 
         sort_val = sorted(d.items(), key=lambda kv: kv[1], reverse = True)
         
-        return sort_val  
-    def frequency(self):
+        return sort_val
+
+    # def indexOfCoincidence(self, text):
+
+    # """ Znajdywanie najczęściej występujących znakow """
+
+    #     lst = self.mostCommon(text)
+    
+    #     return sort_val    
+        
+    def frequency(self,container):
 
         " Znajdywanie częstotliwosci występowania danych liter "
 
-        most = self.mostCommon(self.dataContainer)
+        most = self.mostCommon(container)
         s = sum(x[1] for x in most)
         mostCommonByAll = [(x[0], self.fine_division(x[1], s)) for x in most]
 
